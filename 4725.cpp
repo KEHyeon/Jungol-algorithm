2036f8a95129de966b86adaabac52188e617a7e6
KEHyeon <kth050213@naver.com>
2022-07-24 18:45:11 +0900

    나누기

--- 4725.cpp
+++ 4725.cpp
@@ -1,40 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, ans, arr[100001];
+int n, ans, d[100001], e[100001];
 long long int sum[100001];
 vector<int> dot;
-bool check() {
-	bool flag = 1;
-	if(sum[dot[0]] == sum[dot[1]] - sum[dot[0]]) {
-		if(sum[dot[1]] - sum[dot[0]] == sum[dot[2]] - sum[dot[1]]) {
-			if(sum[dot[2]] - sum[dot[1]] == sum[n] - sum[dot[2]]);
-			else flag = 0;
-		}
-		else flag = 0;
-	}
-	else flag = 0;
-	return flag;
-}
-void bt(int num) {
-	if(dot.size() == 3) {
-		ans += check();
-		return;
-	}
-	for(int i = num + 1; i < n; i++) {
-		dot.push_back(i);
-		bt(i);
-		dot.pop_back();
-	}
-}
 int main() {
 	ios_base :: sync_with_stdio(false); 
 	cin.tie(NULL); 
 	cout.tie(NULL);
 	cin >> n;
-	for(int i = 1; i < n; i++) {
-		cin >> arr[i];
-		sum[i] = sum[i - 1] + arr[i]; 
+	for(int i = 1; i <= n; i++) {
+		int x;
+		cin >> x;
+		sum[i] = sum[i - 1] + x; 
 	}
-	bt(0);
+	if(sum[n] % 4) {
+		cout << 0;
+		return 0;
+	}
+	for(int i = 1; i <= n; i++) {
+		if(sum[i] == sum[n] / 4) d[i] = d[i-1] + 1;
+		else d[i] = d[i - 1];
+	}
+	for(int i = n - 1; i >= 1; i--) {
+		if(sum[i] == sum[n] / 4 * 3) e[i] = e[i + 1] + 1;
+		else e[i] = e[i + 1];
+	}
+	for(int i = 2; i < n; i++) {
+		if(sum[i] == sum[n] / 4 * 2) {
+			ans += d[i-1] * e[i + 1];
+		}
+	}	
 	cout << ans;
 }
